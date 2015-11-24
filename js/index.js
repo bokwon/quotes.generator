14aa28ec69d48423df4cd8f9e7aeb6b741a07c32
Boyoung Kwon <boyoung.kwon@gmail.com>
2015-11-23 16:26:01 -0800

    add quote api call

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  
+  $('#quote_form').submit(function(e){
+    e.preventDefault()
+    var form = $('#quote_form')
+    $.ajax({
+      method: form.attr('method'),
+      url: form.attr('action'),
+      data: form.serialize(),
+      dataType: 'xml',
+      success: function(xml){
+        $('.display_quote').css('display', 'none');
+        $('.display_quote .quote').html($(xml).find("quote").text());
+        $('.display_quote .author').html($(xml).find("author").text());
+        $('.display_quote').fadeIn()
+      }
+    });
+  })
 });
